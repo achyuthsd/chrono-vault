820340219682ebaa3c87d417df2138690d6e4df3
Achyuth S <165375463+achyuthsd@users.noreply.github.com>
2025-11-28 22:31:06 +0530

    Update Loginpage.jsx

--- frontend/src/pages/Loginpage.jsx
+++ frontend/src/pages/Loginpage.jsx
@@ -22,7 +22,7 @@ let res
     {
       localStorage.clear();
       try {
-       res = await axios.post("http://localhost:5000/api/login", {
+       res = await axios.post("/api/login", {
         email,
         password: pass,
       });
